925feb81bdcbffc07373cb2e09700e0d0aeb0369
Harabi Adem <adempirat99@live.fr>
2020-07-09 19:36:58 +0100

    Update and rename db.json to db.js

---
+++ db.js
@@ -0,0 +1,223 @@
+module.exports = {
+  leads: [
+    {
+      id: "3sWX9eJmef",
+      true_author_name: "Kunal  D",
+      lead_content_id: "3sWX9eJmef-1",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      date_created: "2020-06-23 10:41:01",
+      date_updated: "2020-07-05 09:10:44",
+      greeting: "active",
+      icon: "icon",
+      nullid: 78,
+      name: "Virtual Assistant",
+      status: "Draft",
+      title: "Zains Lead",
+      updated_by: 154,
+    },
+    {
+      id: "uGq8CzuluZ",
+      true_author_name: "Kunal  D",
+      lead_content_id: "uGq8CzuluZ-1",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      date_created: "2020-06-23 10:41:01",
+      date_updated: "2020-07-05 09:10:44",
+      greeting: "active",
+      icon: "icon",
+      nullid: 79,
+      name: "Virtual Assistant",
+      status: "Publish",
+      title: "Lead 55",
+      updated_by: 155,
+    },
+    {
+      id: "gQMlfyeCL4",
+      true_author_name: "Kunal  D",
+      lead_content_id: "gQMlfyeCL4-1",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      date_created: "2020-06-23 10:41:01",
+      date_updated: "2020-07-05 09:10:44",
+      greeting: "active",
+      icon: "icon",
+      nullid: 80,
+      name: "Virtual Assistant",
+      status: "Publish",
+      title: "IBZPI",
+      updated_by: 156,
+    },
+    {
+      id: "wPGoJMA4b3",
+      true_author_name: "Kunal  D",
+      lead_content_id: "wPGoJMA4b3-1",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      date_created: "2020-06-23 10:41:01",
+      date_updated: "2020-07-05 09:10:44",
+      greeting: "active",
+      icon: "icon",
+      nullid: 81,
+      name: "Virtual Assistant",
+      status: "Draft",
+      title: "Blank Lead",
+      updated_by: 157,
+    },
+  ],
+
+  lead_contents: [
+    {
+      id: "3sWX9eJmef-1",
+      lead_id: "3sWX9eJmef",
+      author_name: "Kunal  D",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      status: "Draft",
+      title: "Zains Lead",
+    },
+    {
+      id: "uGq8CzuluZ-1",
+      lead_id: "uGq8CzuluZ",
+      author_name: "Kunal  D",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      status: "Publish",
+      title: "Lead 55",
+    },
+    {
+      id: "gQMlfyeCL4-1",
+      lead_id: "gQMlfyeCL4",
+      author_name: "Kunal  D",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      status: "Publish",
+      title: "IBZPI",
+    },
+    {
+      id: "wPGoJMA4b3-1",
+      lead_id: "wPGoJMA4b3",
+      author_name: "Kunal  D",
+      author_photo: "https://devforum.totvs.com.br/attachments/1146?w=48&h=48",
+      status: "Draft",
+      title: "Blank Lead",
+    },
+  ],
+
+  branches: [
+    {
+      id: 6664,
+      lead_content_id: "3sWX9eJmef-1",
+
+      title: "New Branch",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-1", message: "Hello From Bot" }],
+    },
+    {
+      id: 6665,
+      lead_content_id: "3sWX9eJmef-1",
+      title: "Branch1",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-2", message: "Branch1" }],
+    },
+    {
+      id: 6666,
+      lead_content_id: "3sWX9eJmef-1",
+      title: "Branch2",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [
+        { id: "m-3-1", message: "Branch2" },
+        { id: "m-3-2", message: "We are moving you to branch {{new_branch}}" },
+      ],
+    },
+    {
+      id: 6667,
+      lead_content_id: "uGq8CzuluZ-1",
+      title: "New Branch",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-1", message: "Hello From Bot" }],
+    },
+    {
+      id: 6668,
+      lead_content_id: "uGq8CzuluZ-1",
+      title: "Branch1",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-2", message: "Branch1" }],
+    },
+    {
+      id: 6669,
+      lead_content_id: "uGq8CzuluZ-1",
+      title: "Branch2",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [
+        { id: "m-3-1", message: "Branch2" },
+        { id: "m-3-2", message: "We are moving you to branch {{new_branch}}" },
+      ],
+    },
+    {
+      id: 6670,
+      lead_content_id: "gQMlfyeCL4-1",
+      title: "New Branch",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-1", message: "Hello From Bot" }],
+    },
+    {
+      id: 6671,
+      lead_content_id: "gQMlfyeCL4-1",
+      title: "Branch1",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-2", message: "Branch1" }],
+    },
+    {
+      id: 6672,
+      lead_content_id: "gQMlfyeCL4-1",
+      title: "Branch2",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [
+        { id: "m-3-1", message: "Branch2" },
+        { id: "m-3-2", message: "We are moving you to branch {{new_branch}}" },
+      ],
+    },
+    {
+      id: 6673,
+      lead_content_id: "wPGoJMA4b3-1",
+      title: "New Branch",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-1", message: "Hello From Bot" }],
+    },
+    {
+      id: 6674,
+      lead_content_id: "wPGoJMA4b3-1",
+      title: "Branch1",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [{ id: "m-2", message: "Branch1" }],
+    },
+    {
+      id: 6675,
+      lead_content_id: "wPGoJMA4b3-1",
+      title: "Branch2",
+      visible_order: 1,
+      bot_photo:
+        "https://181725.apps.zdusercontent.com/181725/assets/1565789477-b173edd4c84feb4a5efcf55d8082cf69/logo.png",
+      messages: [
+        { id: "m-3-1", message: "Branch2" },
+        { id: "m-3-2", message: "We are moving you to branch {{new_branch}}" },
+      ],
+    },
+  ],
+};
